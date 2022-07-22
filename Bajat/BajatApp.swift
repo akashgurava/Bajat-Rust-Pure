1e2bc9fe1b3e2a2bfb8194c7cb6f1d45ea68467b
Akash <akashgurava@outlook.com>
2022-07-22 22:25:32 +0530

    Initial Commit

---
+++ Bajat/BajatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BajatApp.swift
+//  Bajat
+//
+//  Created by Akash Gurava on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BajatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
